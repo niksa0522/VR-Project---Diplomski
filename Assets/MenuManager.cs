aa09a06f7ba332a01518d9b233e84c81a6fcb60e
niksa0522 <nikola.ilic.bs@elfak.rs>
2022-10-03 16:47:47 +0200

    oculus part finished
    added oculus integration

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -7,24 +7,20 @@ using UnityEngine.InputSystem;
 public class MenuManager : MonoBehaviour
 {
     public GameObject menu;
-    public InputAction showButton;
+    public InputActionProperty showButton;
     public Transform head;
     public float spawnDistance = 2;
 
-    private void Awake()
-    {
-        showButton.started += Pressed;
-    }
-    public void Pressed(InputAction.CallbackContext context)
-    {
-        menu.SetActive(!menu.activeSelf);
-
-        menu.transform.position =
-            head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
-    }
-
     private void Update()
     {
+        if (showButton.action.WasPressedThisFrame())
+        {
+            menu.SetActive(!menu.activeSelf);
+
+            menu.transform.position =
+                head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+            
+        }
         menu.transform.LookAt(new Vector3(head.position.x,menu.transform.position.y,head.position.z));
         menu.transform.forward *= -1;
     }
